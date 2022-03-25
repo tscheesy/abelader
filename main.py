a793821181ebc8abaa8be2ae814d7445d11a2288
tscheesy <100356627+tscheesy@users.noreply.github.com>
2022-03-25 03:28:26 +0100

    - Youtube Object from index Form Input - Check Availability - Get Video Title by API
    - Deleted Design Files

--- main.py
+++ main.py
@@ -2,45 +2,28 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import request
+from pytube import YouTube
 
 import download_file
 
 
 app = Flask(__name__)
 
-"""
-@app.route("/", methods=["GET", "POST"])
-def hello():
-    if request.method == "GET":
-        return render_template("helloform.html")
-    if request.method == "POST":
-        name = request.form["htmlname"]
-        place = request.form["htmlplace"]
-    else:
-        print("Hier hat was nicht funktioniert, sorry. Bitte lade die Seite neu!")
-
-    about_link = url_for("about")
-    return render_template("index.html")
-"""
-
-
 
 @app.route("/", methods=["GET", "POST"])
 def urlreceiver():
 
-    return render_template("index.html")
-
     if request.method == "POST":
-        youtube_link = request_form["youtube_link"]
-        return (youtube_link)
-        print(youtube_link)
-    if request.method == "GET":
-        return ("Fehlermeldung")
-    else:
-        return ("da hät di request method nöd funktioniert")
+        url = request.form['youtube_link']
+        try:
+            youtube_object = YouTube(url)
+            youtube_object.check_availability()
+        except:
+            return "WRONG NUMBER"
 
+        return youtube_object.title
 
-
+    return render_template("index.html")
 
 
 @app.route("/download")
@@ -48,9 +31,6 @@ def urlreceiver():
 def abc():
     success = 1
     return success
-
-
-
 
 
 @app.route("/about")
