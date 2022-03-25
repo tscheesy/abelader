5218b596550ae0659dbf807b27df9e832c727926
tscheesy <100356627+tscheesy@users.noreply.github.com>
2022-03-25 02:35:41 +0100

    - Link Form request - Bootstrap Form & Navbar Header Built index.html - Created download function in separate file - Design Files (logo versions)

---
+++ download_file.py
@@ -0,0 +1,4 @@
+def mp4_return():
+
+    mp4 = "jamaaan mp4 im return !!"
+    return (mp4)
--- main.py
+++ main.py
@@ -3,14 +3,54 @@ from flask import render_template
 from flask import url_for
 from flask import request
 
+import download_file
+
 
 app = Flask(__name__)
 
-
-@app.route("/")
+"""
+@app.route("/", methods=["GET", "POST"])
 def hello():
+    if request.method == "GET":
+        return render_template("helloform.html")
+    if request.method == "POST":
+        name = request.form["htmlname"]
+        place = request.form["htmlplace"]
+    else:
+        print("Hier hat was nicht funktioniert, sorry. Bitte lade die Seite neu!")
+
     about_link = url_for("about")
     return render_template("index.html")
+"""
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def urlreceiver():
+
+    return render_template("index.html")
+
+    if request.method == "POST":
+        youtube_link = request_form["youtube_link"]
+        return (youtube_link)
+        print(youtube_link)
+    if request.method == "GET":
+        return ("Fehlermeldung")
+    else:
+        return ("da hät di request method nöd funktioniert")
+
+
+
+
+
+@app.route("/download")
+@app.route("/download/<id>")
+def abc():
+    success = 1
+    return success
+
+
+
 
 
 @app.route("/about")
